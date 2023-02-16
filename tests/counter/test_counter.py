b0eaa3ad043fbd5e85d5a15e0c3cbeff6a01c6c0
celso-rodrigo <celsorodrigoma@gmail.com>
2023-02-16 17:58:01 -0300

    feat: Added count_ocurrences tests

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,17 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+from pytest import raises
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("data/jobs.csv", 'JaVaScRiPt') == 122
+    assert count_ocurrences("data/jobs.csv", 'python') == 1639
+    assert count_ocurrences("data/jobs.csv", 'invalid_word') == 0
+
+    with raises(FileNotFoundError):
+        assert count_ocurrences("invalid_path", '')
+
+    with raises(TypeError):
+        assert count_ocurrences()
+
+    with raises(AttributeError):
+        assert count_ocurrences("data/jobs.csv", True)
