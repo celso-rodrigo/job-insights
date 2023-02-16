d562a0f53c7514ce68bfb3aa7cb8b92a3dd562f7
celso-rodrigo <celsorodrigoma@gmail.com>
2023-02-16 18:29:32 -0300

    feat: Added brazilian_jobs tests

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,18 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+from pytest import raises
 
 
 def test_brazilian_jobs():
-    pass
+    assert len(read_brazilian_file("tests/mocks/brazilians_jobs.csv")) == 15
+
+    assert read_brazilian_file("tests/mocks/brazilians_jobs.csv")[0] == {
+        "salary": "2000",
+        "title": "Maquinista",
+        "type": "trainee",
+    }
+
+    with raises(FileNotFoundError):
+        assert read_brazilian_file("invalid_path")
+
+    with raises(TypeError):
+        assert read_brazilian_file()
